abf9ab69dacde2313389994f56eba522cc2e7f64
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-08-28 19:21:31 +0300

    add js

--- src/cat-api.js
+++ src/cat-api.js
@@ -10,11 +10,25 @@
 const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
 const API_KEY ='live_nyzZ4QjNtDq03IFxKqTnmDqyZDlfHDPE7tDYK56tjMFQ80XfJTc6gNUHpWGEEVPD';
 
+//api.thecatapi.com/v1/images/search?breed_ids=идентификатор_породы
 
-https://api.thecatapi.com/v1/images/search?breed_ids=идентификатор_породы
-    
-function getBreedCat(breek) {
-    return fetch(`${BASE_URL}?${API_KEY}`).then(response => {
+// const searchParams = new URLSearchParams({
+//     breed_ids: breedId,
+//     api_key:API_KEY,
+// })
+
+function fetchBreeds() {
+  return fetch(`${BASE_URL}?breeds&api_key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+
+function getBreedCat(breedId) {
+    return fetch(`${BASE_URL}?api_key=${API_KEY}&breed_ids=${breedId}`).then(response => {
         if (!response.ok) {
             throw new Error(response.status)
         }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./cat-api"
+
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
