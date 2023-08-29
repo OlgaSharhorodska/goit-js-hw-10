15ae41d11f5a3b751e647916a14c9b7443f2886e
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-08-30 02:35:45 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,51 @@ const catInfo = document.querySelector('.cat-info');
 
 catInfo.classList.add('is-hidden');
 
-breedSelect.addEventListener('change', handlerBreedSelect)
+breedSelect.addEventListener('change', createMarkup)
 
-function handlerBreedSelect() {
-    
+function createMarkup(evt) {
+  loader.classList.replace('is-hidden', 'loader');
+  breedSelect.classList.add('is-hidden');
+  catInfo.classList.add('is-hidden');
+
+  const breedId = evt.currentTarget.value;
+
+  fetchCatByBreed(breedId)
+    .then(data => {
+      loader.classList.replace('loader', 'is-hidden');
+      breedSelect.classList.remove('is-hidden');
+        
+      const { url, breeds } = data[0];
+
+      catInfo.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/><div class="box"><h2>${breeds[0].name}</h2><p>${breeds[0].description}</p><p><strong>Temperament:</strong> ${breeds[0].temperament}</p></div>`;
+      catInfo.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+}
+updateSelect()
+
+function updateSelect(data) {
+  fetchBreeds(data)
+    .then(data => {
+      loader.classList.replace('loader', 'is-hidden');
+
+      let markSelect = data.map(({ name, id }) => {
+        return `<option value ='${id}'>${name}</option>`;
+      });
+      breedSelect.insertAdjacentHTML('beforeend', markSelect);
+      new SlimSelect({
+        select: breedSelect,
+      });
+    })
+    .catch(onError);
+}
+
+
+function onError() {
+  breedSelect.classList.remove('is-hidden');
+  loader.classList.replace('loader', 'is-hidden');
+
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!'
+  );
 }
