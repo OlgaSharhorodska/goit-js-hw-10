6b150f8af815153cea1a0bcaca9dcc0cff0355fd
OlgaSharhorodska <128105592+OlgaSharhorodska@users.noreply.github.com>
2023-08-29 01:36:44 +0000

    Deploying to gh-pages from @ OlgaSharhorodska/goit-js-hw-10@b4e2b52b0f5d3469837d395efc9409bac378b9e6 🚀

--- index.e8cf3a7b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".breed-select"),document.querySelector(".loader"),document.querySelector(".error"),document.querySelector(".cat-info");
-//# sourceMappingURL=index.e8cf3a7b.js.map
--- index.ec0c590a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".breed-select"),document.querySelector(".loader"),document.querySelector(".error"),document.querySelector(".cat-info");
-//# sourceMappingURL=index.ec0c590a.js.map
